9d235bc2154bc98bbd8ddc41a930b8baa9123609
Cole Walker <339783987@GoTVDSB.ca>
2025-03-17 14:12:03 -0400

    Done!

--- if statements assignment/Program.cs
+++ if statements assignment/Program.cs
@@ -6,7 +6,9 @@
         {
             //Part1();
 
-            Part2();
+            //Part2();
+
+            Part3();
         }
         public static void Part1()
         {
@@ -58,15 +60,44 @@
         }
         public static void Part3()
         {
-            string answer1;
-            Console.WriteLine("Who assasinated Aberham Lincon?");
-            answer1= Console.ReadLine();
+            string answer1, answer2, answer3, answer4;
+            Console.WriteLine("1. Who assasinated Aberham Lincon?");
+            answer1 = Console.ReadLine();
             
-            if (answer1 == "john wilkes booth")
+            if (answer1.ToLower() == "john wilkes booth")
                 Console.WriteLine("That's correct! Great job");
 
             else 
                 Console.WriteLine("Not even close...");
+
+            Console.WriteLine("2. What is the smallest prime number?");
+            answer2 = Console.ReadLine();
+
+            if (answer2 == "2" || answer2.ToLower() == "two")
+                Console.WriteLine("Correct! you are a math wizard!");
+
+            else
+                Console.WriteLine("Incorrect...keep in mind, the number is the lowest EVEN number possible.");
+
+            Console.WriteLine("What colour does blue and yellow make?");
+            Console.WriteLine("1. Green    2. Purple   3. Brown");
+            answer3 = Console.ReadLine();
+
+            if (answer3.ToLower() == "green" || answer3 == "1")
+                Console.WriteLine("Correct! You must be an artist!");
+
+            else
+                Console.WriteLine("Incorrect...Sorry");
+
+            Console.WriteLine("Dolphins are mammals - True/False");
+            answer4 = Console.ReadLine();
+
+            if (answer4.ToLower() == "true")
+                Console.WriteLine("Correct! You are so smart!");
+
+            else if (answer4.ToLower() == "false")
+                Console.WriteLine("Nice try! But no.");
+            
         }
     }
 }
